0b1dd50c5fd13d3699eb11402906307ebd4e497c
Riley Griffith <rileyjgriffith@gmail.com>
2020-06-23 00:13:03 -0500

    Initial Commit

---
+++ SpotMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotMe
+//
+//  Created by user173030 on 6/23/20.
+//  Copyright © 2020 Riley Griffith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
