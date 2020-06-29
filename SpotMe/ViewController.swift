d76181c31f30875d1e6554882ad92c2a95adf92a
Riley Griffith <rileyjgriffith@gmail.com>
2020-06-28 19:48:29 -0500

    Added lift find button

--- SpotMe/ViewController.swift
+++ SpotMe/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func liftFinderButton(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
